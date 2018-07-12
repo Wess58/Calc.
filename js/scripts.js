24f37ce2318fae804599d9b090a9859609d72173
Wess58 <wesleymbate@gmail.com>
2018-07-12 12:34:05 +0300

    corrected addition function

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,8 @@ $(document).ready(function(){
             sum += parseInt(noArray[number]);
         }
         console.log(sum);
-
-
-// USER INTERFACE LOGIC
-//$(document).ready(function(){
-  //$("form").submit(function(event){
-    //event.preventDefault();
-    //var noString = $("input#input1").val();
-    //var noArray = noString.split(',');
-    //var sum = 0;
-    //var add = noArray.map( num =>{
-        //return sum += num
-  });
+      }
+      add(noArray)
+      alert(sum);
+    });
 });

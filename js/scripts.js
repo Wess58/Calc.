8ff9aff540fd63a9bebec1fec16dd1c0c9558712
Wess58 <wesleymbate@gmail.com>
2018-07-12 12:33:43 +0300

    created addition function

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,22 @@ $(document).ready(function(){
     event.preventDefault();
     var noString = $("input#input1").val();
     var noArray = noString.split(',');
-    alert(noArray[1]);
+    var sum = 0;
+    var add = function(noArray){
+        for (var number=0;number<=noArray.length-1;number++) {
+            sum += parseInt(noArray[number]);
+        }
+        console.log(sum);
+
+
+// USER INTERFACE LOGIC
+//$(document).ready(function(){
+  //$("form").submit(function(event){
+    //event.preventDefault();
+    //var noString = $("input#input1").val();
+    //var noArray = noString.split(',');
+    //var sum = 0;
+    //var add = noArray.map( num =>{
+        //return sum += num
   });
 });
